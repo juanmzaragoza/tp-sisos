750a0277503c707323c6dfbe38fe9acec322ce12
Hernan de la Fuente <h.dlf@hotmail.com>
2018-05-21 09:48:31 -0300

    Agrego comentarios para que alguien lo siga

--- bin/InterpretO.sh
+++ bin/InterpretO.sh
@@ -56,10 +56,14 @@ showAlert(){
 # FUNCTIONS
 # *************
 
+# Deberia llamar a la funcion de ivan para validar ambiente
+# Devolver 0 (true) o 1 (false)
 validateEnvironment() {
 	return 0
 }
 
+# Valida si el archivo $1 existe
+# Devolver 0 (true) o 1 (false)
 checkIfProcessed() {
 	if fileExits "$1"
 	then
@@ -69,6 +73,8 @@ checkIfProcessed() {
 	fi
 }
 
+# Arma en la variable ROWS la tupla "NOMBRE DE CAMPO"-"TIPO DE CAMPO"
+# Ejemplo : MT_IMPAGO-commax16.2
 # $1: Codigo Pais
 # $2: Codigo sistema
 # Hay que definir antes de llamar al array ROWS
@@ -82,14 +88,13 @@ buildHeader() {
 	done <<< "$HEADER_LIST"
 }
 
-# $1 : Path al archivo
-# $2 : Separador de campos
+# $1 : Path al archivo con informacion del sistema (Ejemplo "A-6-2017-05")
+# $2 : Separador de campos extraido de T1
 # Hay que definir antes de llamar al array VALUES
 buildValues() {
 	LENGHT=${#ROWS[@]}
 	while read -r LINE
 	do
-		# LOCAL_LINE="${LINE::-1}"
 		i=0
 		while (( i < "$LENGHT" ))
 		do
@@ -103,11 +108,13 @@ buildValues() {
 
 processFiles() {
 	CURRENT_DATE=`date +%F`
+	# Checkeo o creo en la carpeta procesados, la sub-carpeta referida a los procesados en el dia current
 	if ! directoryExists "$GRUPO/$PROCESSEDDIR/$CURRENT_DATE"
 	then
 		chmod +w "$GRUPO/$PROCESSEDDIR"
 		mkdir "$GRUPO/$PROCESSEDDIR/$CURRENT_DATE"
 	fi
+	# Recorro todos los archivos aceptados, listos para procesar
 	for FILE_PATH in "${1}/"*; do
 		y=${FILE_PATH%.*}
 		CLEANED_NAME=`echo ${y##*/}`
@@ -117,10 +124,12 @@ processFiles() {
 			showError "Novedad $1 Rechazada. Motivo: El nombre no cumple el patron"
 			return 1
 		fi
+		# Habiendo limpiado el nombre (A-6-2017-05 , por ejemplo, me fijo si ya lo procese en este dia)
 		if checkIfProcessed "$GRUPO/$PROCESSEDDIR/$CURRENT_DATE/$CHECKED_NAME"
 		then
 			mvOrFail "$FILE_PATH" "$GRUPO/$REJECTEDDIR"
 		else
+			# Con el codigo pais y el codigo de sistema, voy a T1 a buscar los separadores de ese sistema
 			COUNTRY_CODE=`echo "$CHECKED_NAME" | sed 's/\(.*\)-.*-.*-.*/\1/'`
 			SYSTEM_CODE=`echo "$CHECKED_NAME" | sed 's/.*-\(.*\)-.*-.*/\1/'`
 			SEPARATORS=`grep "^$COUNTRY_CODE-$SYSTEM_CODE-.*-.*$" "$T1_FILE"`
@@ -130,10 +139,15 @@ processFiles() {
 			else
 				FIELD_SEPARATOR=`echo "$SEPARATORS" | sed 's/^.*-.*-\(.*\)-.*$/\1/'`
 				DECIMAL_SEPARATOR=`echo "$SEPARATORS" | sed 's/^.*-.*-.*-\(.*\)$/\1/'` 
+				# Con el codigo pais y el codigo sistema, armo el header de T2 (es decir, que campos voy a leer y de que tipo son)
 				ROWS=()
 				buildHeader "$COUNTRY_CODE" "$SYSTEM_CODE"
+				# Con los separadores, leo los valores del archivo. Quedna mapeados en el mismo index
 				VALUES=()
 				buildValues "$FILE_PATH" "$FIELD_SEPARATOR"
+				# LA IDEA SERIA LLAMAR AL METODO QUE FORMATEA EL REGISTRO DE SALIDA Y ESCRIBIRLO EN EL ARCHIVO QUE DICE EL ENUNCIADO
+				# EN PRUEBA.SH HAY UNOS INTENTOS QUE ESTUVE HACIENDO, EL METODO OUTPUT REGISTER
+				# DEBERIA MAPEAR EN CADA VARIABLE QUE NECESITA SER ESCRITA, SU VALOR. TENIENDO EN CUENTA EL TIPO DE DATO
 			fi
 		fi
 	done
